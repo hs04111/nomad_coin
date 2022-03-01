9945e430f3a8c3e811d6557bf7535cf27283338d
hs04111 <hs04111@naver.com>
2022-03-01 10:50:31 +0900

    NOMADCOIN: 5.0 Server Setup

--- main.go
+++ main.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/hs04111/nomad_coin/blockchain"
+	"log"
+	"net/http"
 )
 
-func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("First")
-	chain.AddBlock("Second")
-	chain.AddBlock("Third")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
-		fmt.Printf("Hash: %s\n", block.Hash)
-	}
+const port string = ":4000"
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(rw, "Hello!")
 }
 
-// 리팩토링: Singleton pattern을 이용한다
-// Singleton pattern이란? 한 어플리케이션에서 하나의 인스턴스만 사용하는 패턴이다
-// 이를 위해서, blockchain package에서 객체의 생성을 컨트롤한다.
-// main.go에서는 blockchain 인스턴스를 형성할 방법은 GetBlockchain 함수를 사용하는 방법 뿐이다.
+func main() {
+	http.HandleFunc("/", home)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+// go에서는 기본 패키지에서 서버를 열 수 있다.
+// http.ListenAndServe의 첫번째 인자는 포트 주소이고, 두번째는 핸들러
+// log.Fatal은 내부에서 에러가 있으면 Fatal이 작동한다.
+
+// HandleFunc는 Node js의 route와 같다.
+// handler function은 인자로 rw, r이 있는데 rw는 응답을 작성하는 곳
+// r은 요청의 포인터이다. 요청이 클 수도 있으므로, 포인터만 가져오는 것
+
+// rw는 writer로, 여기에 무언가를 쓰려면 Fprint를 사용하여 writer에
+// 작성하여야 한다.
